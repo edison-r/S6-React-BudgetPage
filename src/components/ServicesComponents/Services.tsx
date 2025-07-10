b8e658cb2a670769812fb181f6a52ae6dca7b8be
Edison <ronquillo5@hotmail.com>
2025-07-10 10:15:28 +0200

    feat(services): starting services section

---
+++ src/components/ServicesComponents/Services.tsx
@@ -0,0 +1,9 @@
+export default function Services(){
+    return(
+        <section id="services" className="relative h-screen py-16">
+            <h1 className="absolute left-1/2 -translate-x-1/2 z-0 text-10xl font-bold"> 
+                Services 
+            </h1>
+        </section>
+    );
+}

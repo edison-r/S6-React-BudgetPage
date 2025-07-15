a4afb3964b528c917ba7533ab90f9bb4f56eb938
Edison <ronquillo5@hotmail.com>
2025-07-15 13:15:48 +0200

    fix(requestServiceCard): budgetCard to requestServiceCard
    moved all the code from BudgetCard to requestServiceCard (the good one)

--- src/components/Sections/Quotation.tsx
+++ src/components/Sections/Quotation.tsx
@@ -1,11 +1,11 @@
-import BudgetCard from "../Cards/BudgetCard";
+import RequestServiceCard from "../Cards/RequestServiceCard";
 
 export default function Quotation() {
     return (
         <section id="quotation" className="relative min-h-screen flex flex-col">
             <h1 className="absolute right-[-255px] z-0 text-8xl font-bold">Request quotation</h1>
             <div className="mt-[60px]">
-                <BudgetCard />
+                <RequestServiceCard />
             </div>
         </section>
     );

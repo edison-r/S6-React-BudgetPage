cdf6b4c46b78317d713edc9718900e24fda2fa19
Edison <ronquillo5@hotmail.com>
2025-07-21 17:46:45 +0200

    feat(Name searcher): Name searcher feature done

--- src/components/Sections/QuotationList.tsx
+++ src/components/Sections/QuotationList.tsx
@@ -5,10 +5,15 @@ import BudgetSortControls from "../SharedComponents/BudgetSortControl";
 
 export default function QuotationList({ budgets }: QuotationProps){
     const [sortedBudgets, setSortedBudgets] = useState(budgets);
+    const [searchTerm, setSearchTerm] = useState("");
 
     useEffect(() => {
         setSortedBudgets(budgets);
     }, [budgets]);
+
+    const filteredBudgets = sortedBudgets.filter((budget) =>
+        budget.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
 
     const handleSortByName = () => {
@@ -38,13 +43,22 @@ export default function QuotationList({ budgets }: QuotationProps){
             />
             </svg>
             <h2 className="absolute left-1/3 -translate-x-1/2 top-[455px] z-0 text-8xl font-bold">Ongoing budgets</h2>
-            <BudgetSortControls 
-                onSortByName={handleSortByName}
-                onSortByTotal={handleSortByTotal}
-                onReset={handleReset}
-            />
+            <div className="flex flex-row items-center justify-center gap-6 my-8">
+                <input
+                    type="text"
+                    placeholder="Search by name..."
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    className="absolute top-[150px] px-4 py-2 w-[300px] bg-white/10 text-white  placeholder-white/50 border border-white/20 rounded-md backdrop-blur-sm focus:outline-none"
+                />
+                <BudgetSortControls 
+                    onSortByName={handleSortByName}
+                    onSortByTotal={handleSortByTotal}
+                    onReset={handleReset}
+                />
+            </div>
             <div className="py-32">
-                {sortedBudgets.map((budget, index) => (
+                {filteredBudgets.map((budget, index) => (
                     <div key={index} className="flex flex-col justify-center w-full h-[300px] mb-8 rounded-2xl p-12 backdrop-blur-sm bg-white/5 border border-white/10 shadow-[inset_0_0_5px_rgba(255,255,255,0.1),_0_0_10px_rgba(255,255,255,0.2)]">
                         <BudgetCard budget={budget} />
                     </div>
